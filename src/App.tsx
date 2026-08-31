2eaf84d08bbb53ca9707435759c76edcc6fc3a8d
cs490992-sudo <cs490992@gmail.com>
2026-08-31 17:11:35 +0900

    Wire report routes to actual print page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { AuthProvider } from './context/AuthContext'
 import { AdminRoute, ProtectedRoute } from './components/ProtectedRoute'
 import { AppShell } from './components/AppShell'
 import { LoginPage } from './pages/LoginPage'
-import { ClientPhotos, Completion, Dashboard, NewSurvey, NotFound, PrintReport, Reports, Search, SurveyList } from './pages/Pages'
+import { ClientPhotos, Completion, Dashboard, NewSurvey, NotFound, Reports, Search, SurveyList } from './pages/Pages'
+import { PrintReportPage } from './pages/PrintReportPage'
 import { SurveyEditor } from './pages/SurveyEditor'
 import { SurveyRunPage } from './pages/SurveyRunPage'
 
@@ -14,7 +15,7 @@ export default function App() {
       <Route index element={<Dashboard />} /><Route path="/admin/new" element={<SurveyEditor />} />
       <Route path="/surveys" element={<SurveyList />} /><Route path="/surveys/:surveyId/edit" element={<SurveyEditor />} /><Route path="/surveys/run" element={<SurveyRunPage />} /><Route path="/surveys/run/:surveyId/:roundId" element={<SurveyRunPage />} /><Route path="/surveys/run/:surveyId/:roundId/:clientId" element={<SurveyRunPage />} />
       <Route path="/surveys/completion" element={<Completion />} /><Route path="/surveys/:surveyId/completion" element={<Completion />} /><Route path="/surveys/search" element={<Search />} />
-      <Route path="/reports" element={<Reports />} /><Route path="/reports/:surveyId" element={<Reports />} /><Route path="/reports/print" element={<PrintReport />} /><Route path="/reports/:surveyId/print" element={<PrintReport />} />
+      <Route path="/reports" element={<Reports />} /><Route path="/reports/:surveyId" element={<Reports />} /><Route path="/reports/print" element={<PrintReportPage />} /><Route path="/reports/:surveyId/print" element={<PrintReportPage />} />
       <Route element={<AdminRoute />}><Route path="/admin/client-photos" element={<ClientPhotos />} /></Route>
       <Route path="*" element={<NotFound />} />
     </Route></Route>
